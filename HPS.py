6b962e65da35e6af6693f06637b634d6652c1da1
Stapior <33604831+Stapior@users.noreply.github.com>
2018-12-27 20:00:10 +0100

    Wersja działajaca z argumentem bez zbednych printow

--- HPS.py
+++ HPS.py
@@ -3,78 +3,70 @@ import scipy.signal
 import scipy.fftpack
 import numpy as np
 from matplotlib import pyplot as plt
-import os
-import glob
+import sys
+import warnings
 
-os.chdir('train/')
-files = glob.glob('*.wav')
-show = False  # True
-err = 0
-for filename in files:
-    fs_rate, signal = wavfile.read(filename)  # fs_rate  cxzestotliwosc probkowania
-    l_audio = len(signal.shape)  # liczba kanałow
-    if l_audio == 2:
-        signal = signal.sum(axis=1) / 2  # usrednianie jezeli stereo
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
-    signal = signal[int(len(signal) / 20):-int(len(signal) / 20)]
+show = False
 
-    N = signal.shape[0]  # liczba probek
-    secs = N / float(fs_rate)  # długosc nagrania
-    Ts = 1.0 / fs_rate  # okres pomiedzy probkami
-    t = scipy.arange(0, secs, Ts)  # czas odpowiadający próbkom
+filename = "a.wav"
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+else:
+    print("No file argument")
 
-    signal = signal * np.kaiser(N, 12)  # zastosowanie kaisera
+fs_rate, signal = wavfile.read(filename)  # fs_rate  cxestotliwosc probkowania
+l_audio = len(signal.shape)  # liczba kanałow
+if l_audio == 2:
+    signal = signal.sum(axis=1) / 2  # usrednianie jezeli stereo
 
-    FFT = abs(scipy.fft(signal))  # Transformata FFT
-    freqs = scipy.fftpack.fftfreq(signal.size, Ts)  # wartosci czestotliwosci
-    fft_freqs = np.array(freqs)
+signal = signal[int(len(signal) / 20):-int(len(signal) / 20)]  # obciecie poczatku i konca
 
-    min = 0
-    siec = 0
+N = signal.shape[0]  # liczba probek
 
-    for i, freq in enumerate(freqs):
-        if freq > 35:
-            min = i
-            break
+Ts = 1.0 / fs_rate  # okres pomiedzy probkami
 
-    for i, freq in enumerate(freqs):
-        if freq > 50:
-            siec = i
-            break
+signal = signal * np.kaiser(N, 12)  # zastosowanie kaisera
 
-    FFT[0:min] = 0  # obcięcie dolnych wartosci (pierwszy pik)
-    for i in range(0, 5):
-        FFT[i * siec - 5:i * siec + 5] = 0  # usuniecie 50hz, przydzwiek sieci
+FFT = abs(scipy.fft(signal))  # Transformata FFT
 
-    FFT2 = FFT  # zastosowanie decimate
-    for i in range(1, 6):
-        FFT3 = scipy.signal.decimate(FFT, i)
-        FFT2 = FFT2[range(len(FFT3))] * FFT3
-    FFT_side = FFT2
+freqs = scipy.fftpack.fftfreq(signal.size, Ts)  # wartosci czestotliwosci
+fft_freqs = np.array(freqs)
 
-    pitch = fft_freqs[np.argmax(FFT2)]  # znalezenie dominujacej czestotliwosci ########
-    print(filename)
-    if pitch < 170:
-        print(pitch)
-        print("M")
+min = 0
+siec = 0
 
-        if filename[4] == "K":
-            print("ERROR ##############")
-            print(filename)
-            print(pitch)
-            err += 1
-    else:
-        print("K")
-        print(pitch)
+for i, freq in enumerate(freqs):
+    if freq > 35:
+        min = i
+        break
 
-        if filename[4] == "M":
-            print("ERROR ##############")
-            print(filename)
-            print(pitch)
-            err += 1
+for i, freq in enumerate(freqs):
+    if freq > 50:
+        siec = i
+        break
 
-print(err)
+FFT[0:min] = 0  # obcięcie dolnych wartosci ponizej 30Hz
+for i in range(0, 5):
+    FFT[i * siec - 5:i * siec + 5] = 0  # usuniecie 50hz, przydzwiek sieci
+
+FFT2 = FFT  # zastosowanie decimate
+for i in range(1, 6):
+    FFT3 = scipy.signal.decimate(FFT, i)
+    FFT2 = FFT2[range(len(FFT3))] * FFT3
+FFT_side = FFT2
+
+pitch = fft_freqs[np.argmax(FFT2)]  # znalezenie dominujacej czestotliwosci
+
+if pitch < 170:
+    print("M")
+else:
+    print("K")
+
 if show:
+    secs = N / float(fs_rate)  # długosc nagrania
+    t = scipy.arange(0, secs, Ts)  # czas odpowiadający próbkom
     freqs_side = freqs[range(min * 10)]  # Wyciecie czestotliwosci do wylresu
     fft_freqs_side = np.array(freqs_side)
     # p1 = plt.plot(t, signal, "g")  # plotting the signal
